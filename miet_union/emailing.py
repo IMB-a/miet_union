aaa6a20fe68933665631b5a8c87a5918b6c365d8
Hearthstowner <lazarev989@gmail.com>
2020-02-10 21:27:45 +0300

    fix: try import email_config module

--- miet_union/emailing.py
+++ miet_union/emailing.py
@@ -13,13 +13,16 @@ def send_email(instance, all_emails):
     all_email_addr = []
     for addr in all_emails:
         all_email_addr.append(addr.email)
-    send_mail(
-        subject='Новоя новость на сайте профкома института МИЭТ',
-        message='Hello from django.',
-        # render with dynamic value
-        html_message=render_to_string(
-            'miet_union/mail_template.html', context),
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=all_email_addr,
-        fail_silently=False,
-    )
+    try:
+        send_mail(
+            subject='Новоя новость на сайте профкома института МИЭТ',
+            message='Hello from django.',
+            # render with dynamic value
+            html_message=render_to_string(
+                'miet_union/mail_template.html', context),
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=all_email_addr,
+            fail_silently=False,
+        )
+    except NameError:
+        pass  # email_config is private module
